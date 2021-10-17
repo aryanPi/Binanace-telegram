1bf704e44fcf8197234d23eb20064f47ae0aebd7
Aryan Tomar <53346283+aryanPi@users.noreply.github.com>
2021-10-17 21:58:19 +0530

    third

--- app.js
+++ app.js
@@ -45,6 +45,11 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.status(200).sendFile(path.join(__dirname, "public", "index.html"));
 });
+app.get('/database',async(req,res)=>{
+  functions.getAllData((data) => {
+      res.send(JSON.stringify(data));
+  });
+})
 
 const io = socketIO(server);
 
